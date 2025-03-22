2df1a20a471adfa518de6e40a127eed8a1f46ec9
behrouziyeh <204469443+behrouziyeh@users.noreply.github.com>
2025-03-22 15:57:29 +0330

    feat: implement round logic

--- scripts/main.js
+++ scripts/main.js
@@ -17,8 +17,34 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let HumanChoice = prompt("rock, paper or scissors?", "");
-    if (HumanChoice.match(/rock/i)) return "rock";
-    if (HumanChoice.match(/paper/i)) return "paper";
-    if (HumanChoice.match(/scissors/i)) return "scissors";
+    let humanChoice = prompt("rock, paper or scissors?", "");
+    if (humanChoice.match(/rock/i)) return "rock";
+    if (humanChoice.match(/paper/i)) return "paper";
+    if (humanChoice.match(/scissors/i)) return "scissors";
+}
+
+// initialize Human and Computer score
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`Tie! ${humanChoice} can't beat ${computerChoice}`);
+        return;
+    }
+    function humanWon() {
+        console.log(`You Won! ${humanChoice} beats ${computerChoice}`)
+        humanScore += 1
+    }
+    function humanLost() {
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`)
+        computerScore += 1 
+    }
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'paper' && computerChoice === 'rock') || (humanChoice === scissors && computerChoice === 'paper')) {
+        humanWon()
+        return;
+    } else {
+        humanLost()
+        return;
+    }
 }
